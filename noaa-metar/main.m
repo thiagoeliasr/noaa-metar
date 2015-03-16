5328a6695924d769ab70b99769e2098e269fc13f
Thiago Elias Rezende Silva <thiagopters@sonic.local>
2015-03-16 09:21:47 -0300

    Initial Commit

---
+++ noaa-metar/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  noaa-metar
+//
+//  Created by Thiago Elias Rezende Silva on 16/03/15.
+//  Copyright (c) 2015 Thiago Elias Rezende Silva. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
